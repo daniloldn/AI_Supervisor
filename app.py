2a806e8eb9880b0cb6958213ba42049d9e5ca07c
daniloldn <112765545+daniloldn@users.noreply.github.com>
2026-03-10 15:22:29 +0000

    initalise the app and set up gitignore

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="AI Academic Supervisor")
+
+st.title("AI Industrial Economics Lecturer")
+st.write("Hello, this is a RAG system to learn more about Industrial Economics")
